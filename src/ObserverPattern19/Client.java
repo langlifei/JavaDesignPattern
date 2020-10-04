5ec0101c87b135d9256a2debdb65b37640fae417
langlifei <1084327512@qq.com>
2020-10-04 17:59:14 +0800

    add Observer Pattern

--- src/ObserverPattern19/Client.java
+++ src/ObserverPattern19/Client.java
@@ -7,6 +7,11 @@ import ObserverPattern19.subject.concreteSubject.AllyControllerCenter;
 
 public class Client {
 
+
+    /**
+     * 当被观察的目标发生改变时,通知所有观察该目标的观察者.
+     * @param args
+     */
     public static void main(String[] args) {
         Subject subject = new AllyControllerCenter("金庸群侠");
         Observer play1,play2,play3,play4;
